acd3ba0f81fe6b641d158f1d2aef297fa3769ba8
Steven H <ask@me.com>
2012-10-01 15:40:55 -0700

    cleanup

--- src/js/app.build.js
+++ src/js/app.build.js
@@ -24,6 +24,7 @@
   appDir       : "../",
   baseUrl      : "js",
   dir          : "../../release",
+  name  	   : "app",
   
  /**
 	// single file approach
@@ -86,19 +87,19 @@
    * Use at your own discretion.
    * */
 
-   modules  : [
+   /**modules  : [
     {
-     /**
+     
        * I optimze my app file, because I use
        * my main file to set up my dojoConfig.
        * If doing a single js file optimization,
        * DO NOT include the dojoConfig file
        * to be included. It will blow you up.
        * Optmize the next entry point into your app.
-      */
+     
       name: "app"
-    }
-  ], 
+    } 
+  ],*/ 
 
   /**
    * Will make your css a single line file
